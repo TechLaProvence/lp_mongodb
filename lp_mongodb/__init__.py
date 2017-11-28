90b0547e685b3d0e06982998877270d1263e7de6
ybonnauron <ybonnauron@laprovence.com>
2017-11-28 09:26:26 +0100

    Ajout import config

--- lp_mongodb/__init__.py
+++ lp_mongodb/__init__.py
@@ -1,4 +1,4 @@
-
+from thumbor.config import Config
 Config.define('MONGO_LP_SERVER_HOST', 'localhost', 'MongoDB storage server host', 'MongoDB Storage')
 Config.define('MONGO_LP_SERVER_PORT', 27017, 'MongoDB storage server port', 'MongoDB Storage')
 Config.define('MONGO_LP_SERVER_DB', 'thumbor', 'MongoDB storage server database name', 'MongoDB Storage')
